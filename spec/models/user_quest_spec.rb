f1d765d66610f25fe7ea9c52e42e28b810204954
Joe & Vic Schaeperkoetter <pair+jdcohen9.vschaeperkoetter@devbootcamp.com>
2014-07-24 12:24:00 -0400

    Add user_quest spec test

--- spec/models/user_quest_spec.rb
+++ spec/models/user_quest_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UserQuest, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to(:quest)}
+  it {should belong_to(:user)}
 end
