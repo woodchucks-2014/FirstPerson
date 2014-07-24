9f99c6343141eb8d76de3d690b589f3f57b1906b
Drew Teter <mteter13@gmail.com>
2014-07-24 13:30:24 -0400

    Added a seed file
    Writing the seed file pointed out some gaps in our database. Specifically that Quests need a title and Checkpoints need instructions. The test file is currently untested because we need to make those migrations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,52 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+business = User.new
+business.name = "DBC"
+business.email = "dishes@dbc.com"
+business.password = "test"
+business.password_confirmation = "test"
+business.save!
+
+bar = Location.new
+location.name = "Awesome Bar"
+location.type = "Bar"
+location.lat = 40.7903
+location.lng = 73.9597
+location.save!
+#should locations belong to a user? perhaps through quests and checkpoints
+
+player = User.new
+player.name = "Greg"
+player.email = "greg@dbc.com"
+player.password = "test"
+player.password_confirmation = "test"
+player.save!
+
+quest = Quest.new
+quest.title = "Come Get A Beer!" # Quest neests a title
+quest.creator_id = business.id
+quest.type = "bar"
+quest.end_date = Time.now + 1.day
+quest.save!
+
+beer = Reward.new
+beer.quest_id = quest.id
+beer.xp = 500
+
+checkpoint = Checkpoint.new
+quest.instruction = "Recieve beer" # checkpoints need an instruction
+quest.quest_id = quest.id
+quest.location_id = bar.id
+quest.step_num = 1
+
+greg_gets_a_beer = UserQuest.new
+greg_gets_a_beer.quest_id = quest.id
+greg_gets_a_beer.user_id = greg.id
+
+# user_checkpoints should be automattically created using a before_create method on user_quests
+
+
+
+
 
