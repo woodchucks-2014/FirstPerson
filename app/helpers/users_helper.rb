914b02a668fd42b2ecc6fe2a0a87a942dc362cfd
scoin <greg.piccolo@gmail.com>
2014-07-25 17:32:41 -0400

    more typos wish i didnt have to deploy to heroku to test

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,15 @@
 module UsersHelper
 
   def user_hash_helper
-    user_stuff = {
-    user_stuff[:foursquare_id] = @api.client.user("self")[:id],
-    user_stuff[:photo_url] = @api.client.user("self")[:photo][:prefix] + @api.client.user("self")[:photo][:suffix],
-    user_stuff[:firstname] = @api.client.user("self")[:firstName],
-    user_stuff[:lastname] = @api.client.user("self")[:lastName],
+    user_stuff = {}
+    user_stuff[:foursquare_id] = @api.client.user("self")[:id]
+    user_stuff[:photo_url] = @api.client.user("self")[:photo][:prefix] + @api.client.user("self")[:photo][:suffix]
+    user_stuff[:firstname] = @api.client.user("self")[:firstName]
+    user_stuff[:lastname] = @api.client.user("self")[:lastName]
     user_stuff[:gender] = @api.client.user("self")[:gender]
-	}
+	
+	user_stuff
+
   end
 
 end
