595cdd61cd0638f684af87b91aa21a9a3f59ccf2
& <pair+mdrewt.scoin@devbootcamp.com>
2014-07-25 17:00:30 -0400

    parse_foursquare_json template

--- app/helpers/push_helper.rb
+++ app/helpers/push_helper.rb
@@ -8,6 +8,14 @@ module PushHelper
   end
 
 	def parse_foursquare_json
+    checkin_params = {}
+    checkin_params[:foursquare_id] = params["user"]["id"],
+    checkin_params[:photo_url] = params["user"]["photo"][prefix] + params["user"]["photo"]["suffix"]
+
+    checkin_params[:firstname] = params["user"]["firstname"],
+    checkin_params[:lastname] = params["user"]["lastname"],
+    checkin_params[:gender] = params["user"]["gender"],
+    checkin_params.require("user").permit(:firstname, :lastname, :gender, :photo_url, :foursquare_id)
   end
 
 end
