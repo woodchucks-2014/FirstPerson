9769a7d4435f1ab7be8b799f667505650d71b539
scoin <greg.piccolo@gmail.com>
2014-07-26 13:35:13 -0400

    add to i and to f for pushed params

--- app/helpers/push_helper.rb
+++ app/helpers/push_helper.rb
@@ -12,12 +12,11 @@ module PushHelper
     #user, checkins
     foursquare_fields = {user: {}, location: {}}
 
-    foursquare_fields[:user][:user_id] = User.find_by(foursquare_id: fields["user"]["id"]).id
-
+    foursquare_fields[:user][:user_id] = User.find_by(foursquare_id: fields["user"]["id"].to_i).id
     foursquare_fields[:location][:name] = fields["checkin"]["venue"]["name"]
     foursquare_fields[:location][:venue_type] = fields["checkin"]["venue"]["categories"]["name"]
-    foursquare_fields[:location][:latitude] = fields["checkin"]["venue"]["location"]["lat"]
-    foursquare_fields[:location][:longitude] = fields["checkin"]["venue"]["location"]["lng"]
+    foursquare_fields[:location][:latitude] = fields["checkin"]["venue"]["location"]["lat"].to_f
+    foursquare_fields[:location][:longitude] = fields["checkin"]["venue"]["location"]["lng"].to_f
     foursquare_fields[:location][:address] = fields["checkin"]["venue"]["location"]["formattedAddress"].join(" , ")
 
     foursquare_fields
