a6a4d37553ca6497353ad39747bb48d6a69a270a
scoin <greg.piccolo@gmail.com>
2014-07-25 20:25:48 -0400

    wrote new current user method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,8 +5,15 @@ module UsersHelper
     @user.photo_url = @api.client.user("self")[:photo][:prefix] + @api.client.user("self")[:photo][:suffix]
     @user.firstname = @api.client.user("self")[:firstName]
     @user.lastname = @api.client.user("self")[:lastName]
-    @user.gender = @api.client.user("self")[:gender]
+    @user.gender =@api.client.user("self")[:gender]
+  end
 
+  def current_user2
+  	@user = User.where(id: session[:user_id]).first || User.new
+  end
+
+  def logged_in?
+  	session[:user_id] != nil
   end
 
 end
