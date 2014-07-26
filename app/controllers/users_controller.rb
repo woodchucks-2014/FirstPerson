c5f271efe500f79f39c1be280d2ea94d42bb5d4d
scoin <greg.piccolo@gmail.com>
2014-07-26 14:59:26 -0400

    debugging map js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def checkin_points
-    @points = {}
-    user = User.find(id: session[:user_id]) || User.new
+    @points = []
+    user = User.find(session[:user_id]) || User.new
     user.check_ins.each_with_index do |checkin, i|
       @points.push({lat: checkin.location.latitude, lng: checkin.location.longitude})
     end
