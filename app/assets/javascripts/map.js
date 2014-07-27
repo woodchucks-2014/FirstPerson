d99ca5ae5ad84a78e239ea113f1d79a86e5af9d3
Drew Teter <mteter13@gmail.com>
2014-07-27 00:09:19 -0400

    debugging javascript

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,52 +1,52 @@
-var mapStyle = [
-  {
-    "featureType": "road",
-    "stylers": [
-      { "visibility": "off" }
-    ]
-  },{
-    "featureType": "landscape.man_made",
-    "stylers": [
-      { "visibility": "on" },
-      { "color": "#b8de91" }
-    ]
-  },{
-    "featureType": "landscape.natural",
-    "stylers": [
-      { "color": "#b8de91" }
-    ]
-  },{
-    "featureType": "poi",
-    "stylers": [
-      { "visibility": "off" }
-    ]
-  },{
-    "featureType": "administrative",
-    "stylers": [
-      { "visibility": "off" }
-    ]
-  },{
-    "featureType": "water",
-    "elementType": "labels.text",
-    "stylers": [
-      { "color": "#8080d6" },
-      { "hue": "#1900ff" },
-      { "visibility": "off" }
-    ]
-  },{
-    "featureType": "water",
-    "stylers": [
-      { "color": "#6b80e1" }
-    ]
-  },{
-  }
-]
+// var mapStyle = [
+//   {
+//     "featureType": "road",
+//     "stylers": [
+//       { "visibility": "off" }
+//     ]
+//   },{
+//     "featureType": "landscape.man_made",
+//     "stylers": [
+//       { "visibility": "on" },
+//       { "color": "#b8de91" }
+//     ]
+//   },{
+//     "featureType": "landscape.natural",
+//     "stylers": [
+//       { "color": "#b8de91" }
+//     ]
+//   },{
+//     "featureType": "poi",
+//     "stylers": [
+//       { "visibility": "off" }
+//     ]
+//   },{
+//     "featureType": "administrative",
+//     "stylers": [
+//       { "visibility": "off" }
+//     ]
+//   },{
+//     "featureType": "water",
+//     "elementType": "labels.text",
+//     "stylers": [
+//       { "color": "#8080d6" },
+//       { "hue": "#1900ff" },
+//       { "visibility": "off" }
+//     ]
+//   },{
+//     "featureType": "water",
+//     "stylers": [
+//       { "color": "#6b80e1" }
+//     ]
+//   },{
+//   }
+// ]
 
-var marks;
-$.getJSON( "/checkins", function(data) {
-  marks = data
-});
-console.log(marks)
+// var marks;
+// $.getJSON( "/checkins", function(data) {
+//   marks = data
+// });
+// console.log(marks)
 
 $(document).ready(function() {
 
