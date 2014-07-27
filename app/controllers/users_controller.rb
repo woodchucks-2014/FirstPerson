65ee821b733d0889e08ecd405ddea95c449176de
Drew Teter <mteter13@gmail.com>
2014-07-27 12:00:10 -0400

    added a test login, for testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
   	redirect_to root_path
   end
 
+  def test_login
+    session[:user_id] = 2 # artificial login for testing purposes
+  end
+
   def logout
     session.clear
     redirect_to root_path
