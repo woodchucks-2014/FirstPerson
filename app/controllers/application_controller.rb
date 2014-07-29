d590aba79390106d5c8722d74bad2220ee63cb2c
scoin <greg.piccolo@gmail.com>
2014-07-29 14:37:40 -0400

    move current_user to application controller as before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :current_user
+
+  def current_user
+  	@user = User.find(id: session[:user_id]).first
+  end
+  
 end
