919d7aba98c0cdc9c07783282c3b5cee475c764c
scoin <greg.piccolo@gmail.com>
2014-07-28 22:22:08 -0400

    fixed complete function

--- app/helpers/quests_helper.rb
+++ app/helpers/quests_helper.rb
@@ -8,7 +8,7 @@ module QuestsHelper
 			quest.locations.each do |loc|
 				if loc.foursquare_id == location.foursquare_id
 					checkpoint = quest.checkpoints.find_by(location_id: loc.id)
-					checkpoint.user_checkpoints.find_by(user_id: user.id).complete!
+					checkpoint.user_checkpoints.find_by(user_id: user.id).update(completed: true)
 				end
 			end
 		end
