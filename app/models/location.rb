2c160d206bda1fda2d5beafae42d9e9a0d0586ba
scoin <greg.piccolo@gmail.com>
2014-07-29 19:02:24 -0400

    rip out geocoder, debugging openmaps geocoder api

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,8 +5,8 @@ class Location < ActiveRecord::Base
   has_many :users, through: :check_ins
 
   after_validation :add_address
-  geocoded_by :address
-  before_create :geocode
+  # geocoded_by :address
+  before_create :get_latlng
 
   private
 
@@ -14,4 +14,11 @@ class Location < ActiveRecord::Base
     self.address = [self.street, self.city, self.state, self.zip].join(', ')
   end
 
+  def get_latlng
+    url = "open.mapquestapi.com/geocoding/v1/address"
+    request = "?key=" + ENV["MAP_KEY"] + "&location=" + self.address
+    response = JSON.parse(Net::HTTP.get_response(url,request))
+    puts response["results"]["locations"]["latLng"]
+  end
+#{"results":[{"locations":[{"latLng":{"lng":-74.009107,"lat":40.706486}
 end
