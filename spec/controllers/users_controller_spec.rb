d3d3ce6100633c60a5df2de97df002ab400c895a
Vic Schaeperkoetter <victor.schaeperkoetter@gmail.com>
2014-07-29 17:53:02 -0400

    user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
-  it ""
+  describe "GET index" do
+  	it "returns http success" do
+      get :index
+      expect(response).to be_success
+    end
 
+    it "renders maps#show for a valid user" do
+    	visit 
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
