fa8da41a215d09244fb6042e56aa39081bb40092
scoin <greg.piccolo@gmail.com>
2014-07-29 10:51:47 -0400

    quest check refactor and eager loading

--- app/helpers/quests_helper.rb
+++ app/helpers/quests_helper.rb
@@ -3,11 +3,10 @@ module QuestsHelper
 #need another method in here to check step num, this is rudimentary
 #also need to a quest status to filter out quests that arent valid yet, invalid now, etc..
 	def quest_check(user, location)
-		accepted_quests = user.quests
+		accepted_quests = user.quests.includes(checkpoints:[:user_checkpoints])
 		accepted_quests.each do |quest|
-			quest.locations.each do |loc|
-				if loc.foursquare_id == location.foursquare_id
-					checkpoint = quest.checkpoints.find_by(location_id: loc.id)
+			quest.checkpoints.each do |checkpoint|
+				if checkpoint.location.foursquare_id == location.foursquare_id
 					checkpoint.user_checkpoints.find_by(user_id: user.id).update(completed: true)
 				end
 			end
