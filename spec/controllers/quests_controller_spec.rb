122c22bef129c70aea0698121e6157effe00f0f6
Vic Schaeperkoetter <victor.schaeperkoetter@gmail.com>
2014-07-29 18:23:47 -0400

    feature tests

--- spec/controllers/quests_controller_spec.rb
+++ spec/controllers/quests_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe QuestsController, :type => :controller do
 
-  describe "GET main" do
-    it "returns http success" do
-      get :main
-      expect(response).to be_success
-    end
-  end
+  # describe "GET main" do
+  #   it "returns http success" do
+  #     get :main
+  #     expect(response).to be_success
+  #   end
+  # end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,10 +7,26 @@ RSpec.describe UsersController, :type => :controller do
       expect(response).to be_success
     end
 
-    it "renders maps#show for a valid user" do
-    	visit 
-      get :index
-      expect(response).to render_template :index
-    end
+    # it "renders maps#show for a valid user" do
+    # 	visit 
+    #   get :index
+    #   expect(response).to render_template :index
+    # end
   end
+
+  # describe "GET test_login" do
+  # 	it "should redirect to root path" do
+	 #  	session[:user_id] = 2
+	 #  	get: test_login
+	 #  	expect(response).to redirect_to root_path
+	 #  end
+  # end
+
+  # describe "GET logout" do
+  # 	it "should logout a user" do
+	 #  	session[:user_id] = 2
+	 #  	session[:user_id] = nil
+	 #  	expect(response).to redirect_to root_path
+	 #  end
+  # end
 end
