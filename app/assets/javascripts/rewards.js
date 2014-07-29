a3c58496eeb465c8ef34d36516abf8923a5caec9
Joseph Cohen <jdcohen9@gmail.com>
2014-07-29 19:28:34 -0400

    changed button ids in users sidebar

---
+++ app/assets/javascripts/rewards.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $('').on('click', function(e){
+    e.preventDefault();
+
+    // $.ajax({
+    //   type: "post",
+    //   url: "/create",
+    //   data: $( this ).serialize()
+    // }).done(function(data) {
+    //   $('.create').html(data);
+    // }).fail(function() {
+    //   alert("Please try again");
+    })
+  })
