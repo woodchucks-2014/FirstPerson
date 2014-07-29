5c7c5d2c696ddb97c037ad010f44457225f0305f
& <pair+jdcohen9.vschaeperkoetter@devbootcamp.com>
2014-07-28 20:33:30 -0400

    create reward route & test

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -3,6 +3,15 @@ class RewardsController < ApplicationController
     @reward = Reward.new
   end
 
+  def create
+    # @quest = Quest.find(params[:id])
+    @reward = Reward.new
+    @reward.save
+    render :create
+
+  end
+
+
   # the page for a particular award
   # shows a picture, has a description
   # says which quest it's from
--- spec/controllers/rewards_controller_spec.rb
+++ spec/controllers/rewards_controller_spec.rb
@@ -3,9 +3,28 @@ require 'rails_helper'
 RSpec.describe RewardsController, :type => :controller do
 
   describe 'GET #new' do
-    it 'assigns a new Reward to @reward' do
+    it 'assigns a new reward to @reward' do
       get :new
       expect(assigns(:reward)).to be_a_new(Reward)
     end
   end
+
+
+
+  describe 'POST #create' do
+    it 'creates a new reward' do
+      post :create, reward: FactoryGirl.attributes_for(:reward)
+      expect(response).to be_success
+    end
+  end
+
+  # describe 'POST #create' do
+  #   let(:reward) {create :reward}
+  #   context "with valid attributes" do
+  #     it "saves the new reward in the database" do
+  #       expect{post :create, reward: attributes_for(:reward)}.to change(Reward, :count).by(1)
+
+  #     end
+  #   end
+  # end
 end
