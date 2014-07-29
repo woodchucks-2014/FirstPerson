9faf65604217338cf9fe8d7c7707fe3e3afd00a2
Drew Teter <mteter13@gmail.com>
2014-07-29 18:39:26 -0400

    users

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe UsersHelper, :type => :helper do
 
   it "can tell if someone is not logged in" do
     session[:user_id] = nil
-    expect(logged_in?).to be_false
+    expect(logged_in?).to eq(false)
   end
 
   it "can tell if someone is logged in" do
     session[:user_id] = 1
-    expect(logged_in?).to be_true
+    expect(logged_in?).to eq(true)
   end
 
 end
