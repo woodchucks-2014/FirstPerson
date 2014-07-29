2d7ace53049f2f6a0e4a12999ca9fed0f60cac03
scoin <greg.piccolo@gmail.com>
2014-07-29 10:56:15 -0400

    decided not to eager load user checkpoints because it may not be needed

--- app/helpers/quests_helper.rb
+++ app/helpers/quests_helper.rb
@@ -3,7 +3,7 @@ module QuestsHelper
 #need another method in here to check step num, this is rudimentary
 #also need to a quest status to filter out quests that arent valid yet, invalid now, etc..
 	def quest_check(user, location)
-		accepted_quests = user.quests.includes(checkpoints:[:user_checkpoints])
+		accepted_quests = user.quests.includes(:checkpoints)
 		accepted_quests.each do |quest|
 			quest.checkpoints.each do |checkpoint|
 				if checkpoint.location.foursquare_id == location.foursquare_id
