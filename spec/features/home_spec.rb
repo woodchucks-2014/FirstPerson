61805d1a21b00b34cf4ad4c7308c36041df28d0b
Joseph Cohen <jdcohen9@gmail.com>
2014-07-30 10:42:50 -0400

    updated home_spec test

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,21 +4,15 @@ RSpec.describe "home" do
   describe "Non-logged in user sees login button" do
     it "by visiting homepage" do
       visit '/'
-      expect(page).to have_css("#sign_in_link")
+      expect(page).to have_css("#enter_link")
     end
   end
 
   context 'buttons' do
     it "has signin button" do
       visit '/'
-      expect(page).to have_selector("a", text: "Sign In")
+      expect(page).to have_selector("a", text: "Enter")
     end
-
-    it "has signup button" do
-      visit '/'
-      expect(page).to have_selector("a", text: "Sign Up")
-    end
-
   end
 
 end
