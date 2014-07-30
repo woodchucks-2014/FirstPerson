b69e575bd1756f8fa07913cfe515875e8ef4ed0f
Vic Schaeperkoetter <victor.schaeperkoetter@gmail.com>
2014-07-30 10:04:07 -0400

    Pull request #91

--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -12,10 +12,16 @@ RSpec.describe Quest, :type => :model do
   # 	user = FactoryGirl.create(:user, user_id: 10)
   # 	user_quest = FactoryGirl.create(:user_quest, user_id: 10, quest_id: 10)
   # 	quest = FactoryGirl.create(:quest, quest_id: 10)
+  
   describe "Quest Model" do
     it "returns a location using the location method" do
       quest = FactoryGirl.create(:quest)
       expect(quest.locations.first).to eq(quest.location)
+    end
+
+    it "sets user status to 'full' when equal or over user limit" do
+      quest = FactoryGirl.create(:quest, user_limit: 1)
+      expect(quest.userstatus).to eq("full")
     end
   end
 end
