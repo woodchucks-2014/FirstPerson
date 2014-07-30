eeb0138c1ed027a13e72c9a7bb3caa419b07b2df
Drew Teter <mteter13@gmail.com>
2014-07-30 04:14:05 -0400

    custom markers

--- app/helpers/build_hash_helper.rb
+++ app/helpers/build_hash_helper.rb
@@ -1,18 +1,42 @@
 module BuildHashHelper
 
-  def build_quests_hash(quests)
+  def imager(style)
+    case style
+    when "default"
+      return "http://img3.wikia.nocookie.net/__cb20120626061259/secretworld/images/d/d2/Mission-action.png"
+    when "completed checkpoint"
+    when "checkpoint"
+    when "quest"
+    when "completed quest"
+    when "checkin"
+    end
+  end
+
+  def build_quests_hash(quests, style = "default")
+    img_url = imager(style)
     Gmaps4rails.build_markers(quests) do |quest, marker|
       marker.lat quest.locations.first.latitude
       marker.lng quest.locations.first.longitude
+      marker.picture({
+        url: img_url,
+        width:  36,
+        height: 36
+      })
       marker.infowindow "<iframe src='/accept?quest_id=#{quest.id}' style='scrolling=no;'></iframe>"
     end
   end
 
-  def build_checkpoints_hash(checkins)
+  def build_checkpoints_hash(checkins, style = "default")
+    img_url = imager(style)
     checkins = checkins.map {|checkin| checkin.location}
     Gmaps4rails.build_markers(checkins) do |checkin, marker|
       marker.lat checkin.latitude
       marker.lng checkin.longitude
+      marker.picture({
+        url: img_url,
+        width:  36,
+        height: 36
+      })
       marker.infowindow checkin.address
     end
   end
