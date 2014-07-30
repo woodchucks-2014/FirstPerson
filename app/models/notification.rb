09f05ab97401a2e9aea5e08ebb09f3f6a5a742dc
Drew Teter <mteter13@gmail.com>
2014-07-29 23:57:11 -0400

    notifications can hash themselves

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,7 @@
 class Notification < ActiveRecord::Base
   belongs_to :user
+
+  def to_js
+    {title: self.title, content: self.content}
+  end
 end
--- db/migrate/20140730030342_create_notifications.rb
+++ db/migrate/20140730030342_create_notifications.rb
@@ -2,7 +2,7 @@ class CreateNotifications < ActiveRecord::Migration
   def change
     create_table :notifications do |t|
       t.integer :user_id
-      t.string :type
+      t.string :title
       t.text :content
     end
   end
