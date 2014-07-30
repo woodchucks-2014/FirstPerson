d21084c7bf47e9cb74a02b9bfab9d35ddae076c0
Drew Teter <mteter13@gmail.com>
2014-07-29 23:12:10 -0400

    added notification migration, test, model

--- spec/models/check_in_spec.rb
+++ spec/models/check_in_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe CheckIn, :type => :model do
   it {should belong_to(:user)}
 
   it "sets a user's xp at a check-in to a default of 0" do
+    puts "\n\n\n\n********************\n\n\n\n"
     ex = FactoryGirl.build(:check_in)
     ex.set_defaults
     expect(ex.xp).to eq(0)
