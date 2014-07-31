1ef7de7d3a8fcb3abbb5c9c7feb2a52759fb0da0
Joseph Cohen <jdcohen9@gmail.com>
2014-07-31 09:29:36 -0400

    remove comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,17 +11,5 @@ RSpec.describe User, :type => :model do
   it {should have_many(:check_ins)}
   it {should have_many(:locations).through(:check_ins)}
   it {should have_many(:notifications)}
-
-
-  # build some users, assign them xp, list them in descending order
-
-  it 'orders users by total xp' do
-    user1 = FactoryGirl.build(:user, :total_xp => 400)
-    user2 = FactoryGirl.build(:user, :total_xp => 200)
-    users = User.sort_users
-    expect(users).to eq([user1, user2])
-  end
 end
 
-# we're going to have to build a another method in the user model that outputs
-# that array, and then write this test based off that.
